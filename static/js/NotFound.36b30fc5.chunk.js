252e319c6d0a247f667230c39613817ba38e6ffd
= <=>
2023-02-01 16:32:13 +0100

    Updates

---
+++ static/js/NotFound.36b30fc5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,a,t){t.r(a);t(2791);var u=t(6662),c=t(184);a.default=function(){return(0,c.jsx)(u.Z,{})}}}]);
+//# sourceMappingURL=NotFound.36b30fc5.chunk.js.map
